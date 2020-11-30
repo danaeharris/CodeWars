618f2094bf271ece668a7569423115bba04f620e
danaeharris <rvdbinns@gmail.com>
2020-11-30 13:08:16 -0500

    adding problem instructions to each kata.

--- smallest-unused-id.js
+++ smallest-unused-id.js
@@ -1,3 +1,13 @@
+// Hey awesome programmer!
+
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+// Go on and code some pure awesomeness!
+
 function nextId(ids) {
   //call id.every to see if any id = 0 if so, add 1...
   let n = 0;
