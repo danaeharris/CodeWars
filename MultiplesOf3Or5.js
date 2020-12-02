9588aa51ad408450addb5151b85b4d056e7e7504
danaeharris <rvdbinns@gmail.com>
2020-12-02 14:24:04 -0500

    Adding solution to MultiplesOf3Or5

--- MultiplesOf3Or5.js
+++ MultiplesOf3Or5.js
@@ -4,3 +4,20 @@
 
 // Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
 
+function solution(number) {
+    let sumArray = [];
+    let num = number - 1;
+    while (num > 2) {
+        if (num % 5 === 0) {
+            sumArray.push(num);
+        } else if (num % 3 === 0) {
+            sumArray.push(num);
+        }
+        num = num - 1
+    }
+    if (sumArray.length > 0) {
+        return sumArray.reduce((a, b) => a + b)
+    } else {
+        return 0;
+    }
+}
