9dde050116ed9d5470778c3ec4880967e31dac77
danaeharris <rvdbinns@gmail.com>
2020-12-14 17:22:28 -0500

    Adding instructions to beginning of katas.

--- jumpingNumbers.js
+++ jumpingNumbers.js
@@ -1,3 +1,23 @@
+// Definition
+// Jumping number is the number that All adjacent digits in it differ by 1.
+
+// Task
+// Given a number, Find if it is Jumping or not .
+
+// Notes
+// Number passed is always Positive .
+
+// The difference between ‘9’ and ‘0’ is not considered as 1 .
+
+// All single digit numbers are considered as Jumping numbers.
+
+// jumpingNumber(79) ==> return "Not!!"
+// Explanation:
+// Adjacent digits don't differ by 1
+// jumpingNumber(23) ==> return "Jumping!!"
+// Explanation:
+// Adjacent digits differ by 1
+
 let jumpingNumber = (n) => {
     let numArray = n.toString().split('');
     let previousNum = numArray[0] - 1;
