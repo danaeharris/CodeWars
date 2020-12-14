c9c0962d85c2b68eac8ddfdef243955e993985c9
danaeharris <rvdbinns@gmail.com>
2020-12-14 16:35:47 -0500

    Update jumpingNumbers.js

--- jumpingNumbers.js
+++ jumpingNumbers.js
@@ -1,3 +1,14 @@
 let jumpingNumber = (n) => {
-    //your code here
+    let numArray = n.toString().split('');
+    let previousNum = numArray[0] - 1;
+    let jumping = true;
+    numArray.forEach((num, i)=>{
+        let currentNum = parseInt(num);
+        if (currentNum + 1 === previousNum || currentNum - 1 === previousNum){
+            previousNum = currentNum;
+        }else {
+            jumping = false;
+        }
+    })
+    return jumping ? 'Jumping!!' : "Not!!";
   }
